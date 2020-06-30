26b9aee84f94283737c4e5ffa5249aaadbe5d545
fluffysaur <tanyijia@gmail.com>
2020-07-01 03:51:01 +0800

    Font tweaks, backgroudn video, and background audio

---
+++ public/js/bgaudio.js
@@ -0,0 +1,28 @@
+(function ($) {
+"use strict";
+var audio = document.getElementById("bgaudio");
+
+$(window).on('load', function() {
+  audio.volume = 0.3;
+});
+
+$('.mutebtn').on('click', function() {
+  $('.muteicon').toggleClass('fa-volume-mute');
+  $('.muteicon').toggleClass('fa-volume-up');
+  if (audio.paused == false) {
+    audio.pause();
+  } else {
+    audio.play();
+  }
+});
+
+// Seamless Audio Loop Fix
+audio.addEventListener('timeupdate', function(){
+    var buffer = .44
+    if(this.currentTime > this.duration - buffer){
+        this.currentTime = 0
+        this.play()
+    }
+});
+
+})(jQuery);	
