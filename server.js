ae23613fa2f7bf228c233e41fdc7dd4d1781fe46
fluffysaur <tanyijia@gmail.com>
2020-08-01 00:01:47 +0800

    server revamp

--- server.js
+++ server.js
@@ -18,23 +18,7 @@ const express = require('express')
     port : process.env.MYSQLPORT
 });
 
-var options = {
-    host: 'tplace.xyz',
-    port: 5000,
-    path: '/api/grid'
-},
-body = '';
-
 app.use(express.static(path.join(__dirname, 'public')));
-
-// app.get(options, function(responseFromRemoteAPI) {
-//     responseFromRepoteApi.on('data', function(chunk) {
-//         body += chunk;
-//     });
-//     responseFromRemoteAPI.on('end', function() {
-//         response.writeHead(200, {"grid": grid})
-//     })
-// });
 
 app.get('/:id', function (req, res) {
     res.status(404).redirect('/404.html');
@@ -49,74 +33,85 @@ server.listen(port, function(){
     console.log (`Server listening on port ${port}.`);
 });
 
+var igDetails = [];
+var igImages = [];
+var igReviews = [];
+
+pool.getConnection(function(err, connection){
+    if (err) throw err;
+    connection.query(`SELECT * FROM ig_details`, function (err, result1) {
+        if (err) throw err;
+        if (result1.length < 1) {
+            console.log(`Database Empty.`);
+            connection.release();
+        } else {
+            igDetails = result1;
+            console.log(`Step 1 Success`);
+            connection.query(`SELECT * FROM reviews`, function (err, result2) {
+                if (err) throw err;
+                igReviews = result2;
+                console.log(`Step 2 Success`);
+            connection.query(`SELECT * FROM images_videos`, function (err, result3) {
+                if (err) throw err;
+                igImages = result3;
+                console.log(`Step 3 Success`);
+                // connection.query(`SELECT * FROM social_media WHERE name='${targetIG}'`, function (err, result3) {
+                //     if (err) throw err;
+                //     socialMedia = result3;
+                //     console.log(`Step 3 Success`);
+                    connection.release();
+                });
+            });
+        };
+    });
+});
+
+
 socketio.listen(server).on('connection', function (socket) {
     console.log(`Got a connection on socket: ${socket}.`);
 
     socket.on('reqIG', function(targetIG) {
         console.log(`Retrieving IG data for ${targetIG}`);
-        pool.getConnection(function(err, connection){
-            if (err) throw err;
-            var igDetails = [];
-            var igImages = [];
-            var igReviews = [];
-            // var socialMedia = [];
-            connection.query(`SELECT * FROM ig_details WHERE name='${targetIG.replace(/\'/g, `''`)}'`, function (err, result1) {
-                if (err) throw err;
-                if (result1.length < 1) {
-                    socket.emit('retIG', "empty");
-                    console.log(`Step 1 Failed: IG does not exist.`);
-                    connection.release();
-                } else {
-                    igDetails = result1;
-                    console.log(`Step 1 Success`);
-                    connection.query(`SELECT * FROM reviews WHERE name='${targetIG.replace(/\'/g, `''`)}'`, function (err, result2) {
-                        if (err) throw err;
-                        igReviews = result2;
-                        console.log(`Step 2 Success`);
-                    connection.query(`SELECT * FROM images_videos WHERE name='${targetIG.replace(/\'/g, `''`)}'`, function (err, result3) {
-                        if (err) throw err;
-                        igImages = result3;
-                        console.log(`Step 3 Success`);
-                        // connection.query(`SELECT * FROM social_media WHERE name='${targetIG}'`, function (err, result3) {
-                        //     if (err) throw err;
-                        //     socialMedia = result3;
-                        //     console.log(`Step 3 Success`);
-                            socket.emit('retIG', {ig: igDetails, reviews: igReviews, img: igImages});
-                            connection.release();
-                        });
-                    });
-                };
-            });
-        });
-    })
+        var det = [], img = [], rev = [];
+
+        for (var i=0;i<igDetails.length;i++){
+            if (igDetails[i].name == targetIG) {
+                det.push(igDetails[i]);
+            }
+        }
+        for (var i=0;i<igImages.length;i++){
+            if (igImages[i].name == targetIG) {
+                img.push(igImages[i]);
+            }
+        }
+        for (var i=0;i<igReviews.length;i++){
+            if (igReviews[i].name == targetIG) {
+                rev.push(igReviews[i]);
+            }
+        }
+
+        if (det.length == 0) {
+            socket.emit('retIG', "empty");
+            console.log(`Single IG Request Failed: IG does not exist.`);
+        }
+
+        socket.emit('retIG', {'ig': det, 'reviews': rev, 'img': img});
+    });
 
     socket.on('reqIGPage', function(targetCategory) {
         console.log(`Retrieving IG data for ${targetCategory}`);
-        pool.getConnection(function(err, connection){
-            if (err) throw err;
-            var igDetails = [];
-            var igImages = [];
-            // var socialMedia = [];
-            connection.query(`SELECT * FROM ig_details WHERE category='${targetCategory}'`, function (err, result1) {
-                if (err) throw err;
-                if (result1.length < 1) {
-                    socket.emit('retIG', "empty");
-                    console.log(`Step 1 Failed: No IGs in category exist.`);
-                    connection.release();
-                } else {
-                    igDetails = result1;
-                    console.log(`Step 1 Success`);
-                    connection.query(`SELECT * FROM images_videos WHERE name in (SELECT name FROM ig_details WHERE category='${targetCategory}')`, function (err, result2) {
-                        if (err) throw err;
-                        igImages = result2;
-                        console.log(`Step 2 Success`);
-                        socket.emit('retIGPage', {ig: igDetails, img: igImages});
-                        connection.release();
-                    });
-                };
-            });
-        });
-    })
+        var det = [], img = [];
+        for (var i=0;i<igDetails.length;i++) {
+            if (igDetails[i].category == targetCategory) {
+                det.push(igDetails[i]);
+                name = igDetails[i].name;
+                for (var x=0;x<igImages.length;x++) {
+                    if (igImages[x].name == igDetails[i].name) img.push(igImages[x]);
+                }
+            }
+        }
+        socket.emit('retIGPage', {'ig': det, 'img': img});
+    });
     
     socket.on('disconnect', function(){
         console.log(`Socket: ${socket} disconnected.`);
